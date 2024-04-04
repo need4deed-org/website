b4d60853c2ce51cb614229298e6b34b39a498ed7
Arturas Mickiewicz <arturas.mickiewicz@need4deed.org>
2024-04-04 22:46:09 +0200

    fixed p tag height and removed from carousel container

--- src/components/VolunteerOpportunity/VolunteerOpportunity.tsx
+++ src/components/VolunteerOpportunity/VolunteerOpportunity.tsx
@@ -1,70 +1,70 @@
-import { useTranslation } from 'react-i18next';
-import './VolunteerOpportunity.css';
+import { useTranslation } from "react-i18next";
+import "./VolunteerOpportunity.css";
 
 function VolunteerOpportunity() {
   const { t } = useTranslation();
 
   return (
     <div
-      id='carouselExampleAutoplaying'
-      className='carousel slide volunteer-opportunity-container'
-      data-bs-ride='carousel'
+      id="carouselExampleAutoplaying"
+      className="carousel slide volunteer-opportunity-container"
+      data-bs-ride="carousel"
     >
-      <div className='volunteer-opportunity-heading'>
-        <h3 id='volunteer-opportuinities-section'>
-          {t('workingWithRefugees')}
+      <div className="volunteer-opportunity-heading">
+        <h3 id="volunteer-opportuinities-section">
+          {t("workingWithRefugees")}
         </h3>
       </div>
-      <div className='carousel-inner fixed-height'>
-        <div className='carousel-item active' data-bs-interval='6000'>
-          <div className='become-a-mentor-img volunteer-opportunity-img'>
-            <div className='volunteer-opportunity-text'>
-              <h5>{t('volunteeringProjects.overComeObstaclesHeading')}</h5>
-              <p>{t('volunteeringProjects.overComeObstaclesSubheading')}</p>
-              <a href='#' className='btn btn-light learn-more'>
-                {t('learnMoreButton')}
+      <div className="carousel-inner">
+        <div className="carousel-item active" data-bs-interval="6000">
+          <div className="become-a-mentor-img volunteer-opportunity-img">
+            <div className="volunteer-opportunity-text">
+              <h5>{t("volunteeringProjects.overComeObstaclesHeading")}</h5>
+              <p>{t("volunteeringProjects.overComeObstaclesSubheading")}</p>
+              <a href="#" className="btn btn-light learn-more">
+                {t("learnMoreButton")}
               </a>
             </div>
           </div>
         </div>
-        <div className='carousel-item' data-bs-interval='6000'>
-          <div className='child-care-img volunteer-opportunity-img'>
-            <div className='volunteer-opportunity-text'>
-              <h5>{t('volunteeringProjects.childCareHeading')}</h5>
-              <p>{t('volunteeringProjects.childCareSubheading')}</p>
-              <a href='#' className='btn btn-light learn-more'>
-                {t('learnMoreButton')}
+        <div className="carousel-item" data-bs-interval="6000">
+          <div className="child-care-img volunteer-opportunity-img">
+            <div className="volunteer-opportunity-text">
+              <h5>{t("volunteeringProjects.childCareHeading")}</h5>
+              <p>{t("volunteeringProjects.childCareSubheading")}</p>
+              <a href="#" className="btn btn-light learn-more">
+                {t("learnMoreButton")}
               </a>
             </div>
           </div>
         </div>
-        <div className='carousel-item' data-bs-interval='6000'>
-          <div className='sport-programmes-img volunteer-opportunity-img'>
-            <div className='volunteer-opportunity-text'>
-              <h5>{t('volunteeringProjects.sportProgramHeading')}</h5>
-              <p>{t('volunteeringProjects.sportProgramSubheading')}</p>
-              <a href='#' className='btn btn-light learn-more'>
-                {t('learnMoreButton')}
+        <div className="carousel-item" data-bs-interval="6000">
+          <div className="sport-programmes-img volunteer-opportunity-img">
+            <div className="volunteer-opportunity-text">
+              <h5>{t("volunteeringProjects.sportProgramHeading")}</h5>
+              <p>{t("volunteeringProjects.sportProgramSubheading")}</p>
+              <a href="#" className="btn btn-light learn-more">
+                {t("learnMoreButton")}
               </a>
             </div>
           </div>
         </div>
       </div>
       <button
-        className='carousel-control-prev'
-        type='button'
-        data-bs-target='#carouselExampleAutoplaying'
-        data-bs-slide='prev'
+        className="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExampleAutoplaying"
+        data-bs-slide="prev"
       >
-        <span className='carousel-control-prev-icon' aria-hidden='true'></span>
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
       </button>
       <button
-        className='carousel-control-next'
-        type='button'
-        data-bs-target='#carouselExampleAutoplaying'
-        data-bs-slide='next'
+        className="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExampleAutoplaying"
+        data-bs-slide="next"
       >
-        <span className='carousel-control-next-icon' aria-hidden='true'></span>
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
       </button>
     </div>
   );
