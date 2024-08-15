47fd5f93e05c3602159bcd4ae501bb0c721ade57
Arturas Mickiewicz <arturas.mickiewicz@need4deed.org>
2024-08-15 09:03:00 +0000

    Update event page content

--- src/components/Event/Event831.tsx
+++ src/components/Event/Event831.tsx
@@ -14,8 +14,29 @@ export default function Event831() {
         <div>
           <p>{t("event.paras.para1")}</p>
           <p>{t("event.paras.para2")}</p>
-          <p>{t("event.paras.para3")}</p>
-          <p>{t("event.paras.para4")}</p>
+          <p className="event-timeslot">
+            {t("event.paras.para3.morning.timeslot")}
+          </p>
+          <p>{t("event.paras.para3.morning.title1")}</p>
+          <p className="event-timeslot">
+            {t("event.paras.para3.day.timeslot")}
+          </p>
+          <p>{t("event.paras.para3.day.title1")}</p>
+          <p>{t("event.paras.para3.day.title2")}</p>
+          <p>{t("event.paras.para3.day.title3")}</p>
+          <p className="event-timeslot">
+            {t("event.paras.para3.afternoon.timeslot")}
+          </p>
+          <p>{t("event.paras.para3.afternoon.title1")}</p>
+          <p>{t("event.paras.para3.afternoon.title2")}</p>
+          <p>{t("event.paras.para3.afternoon.title3")}</p>
+          <p className="event-timeslot">
+            {t("event.paras.para3.all.timeslot")}
+          </p>
+          <p>{t("event.paras.para3.all.title1")}</p>
+          <p>{t("event.paras.para3.all.title2")}</p>
+
+          <p className="event-location">{t("event.paras.para4")}</p>
         </div>
       </div>
       <div className="event-rsvp">
