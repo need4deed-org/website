e9396d9b5007cfe095fb91c8fe25cf2a766c7ad5
Arturas Mickiewicz <arturas.mickiewicz@need4deed.org>
2025-01-05 10:23:52 +0100

    update: get back to email domain list

--- src/components/form/validators.ts
+++ src/components/form/validators.ts
@@ -1,11 +1,36 @@
 import { ValidationError } from "@tanstack/react-form";
 
 export function validateRACEmail(email: string, errorMsg = "Bad email.") {
-  const AUTHORIZED_DOMAINS: string[] = [];
+  const AUTHORIZED_DOMAINS = [
+    "lfg-b.de",
+    "berliner-stadtmission.de",
+    "awo-mitte.de",
+    "prisod-wohnen.de",
+    "albatrosggmbh.de",
+    "gu-oberhafen.de",
+    "heroeurope.com",
+    "tamaja-gu.de",
+    "volkssolidaritaet.de",
+    "unionhilfswerk.de",
+    "cjd.de",
+    "centro-hotels.de",
+    "stephanus.org",
+    "city54.de",
+    "milaa-berlin.de",
+    "stk118.de",
+    "drk-mueggelspree.de",
+    "laf.berlin.de",
+    "ejf.de",
+    "diakonie-stadtmitte.de",
+    "tamaja.de",
+    "pgssoziales.de",
+    "caritas-berlin.de",
+    "need4deed.org",
+  ];
   return new Promise<ValidationError>((resolve) => {
     const [, domain] = email.split("@");
     setTimeout(
-      () => resolve(AUTHORIZED_DOMAINS.includes(domain) ? errorMsg : undefined),
+      () => resolve(AUTHORIZED_DOMAINS.includes(domain) ? undefined : errorMsg),
       500,
     );
   });
