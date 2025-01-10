2dd99a78cdd541431645b1ae96a45f24f42283c8
aricionur <aricionur@gmail.com>
2025-01-10 22:21:34 +0000

    Added new FAQs subpage component, questions and answers for all languages

--- src/pages/Subpage.tsx
+++ src/pages/Subpage.tsx
@@ -17,6 +17,7 @@ import { showEvent, urlApiOpportunity } from "../config/constants";
 import { FormType, Lang, OpportunityType, Subpages } from "../config/types";
 import AppContainerContext from "../contexts/AppContainerContext";
 import { getImageUrl, isEnumValue, setLangDirection } from "../utils";
+import FAQs from "../components/FAQs/FAQs";
 
 interface Props {
   type: Subpages;
@@ -144,6 +145,8 @@ function Subpage({ type }: Props) {
         return <Announcement />;
       case Subpages.EVENT:
         return <EventHolidayGift />;
+      case Subpages.FAQS:
+        return <FAQs />;
       default:
         return null;
     }
