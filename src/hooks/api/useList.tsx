8a7f05c70bfecd0f5ce0d5f003e3145ed9ff73ab
Arturas Mickiewicz <arturas.mickiewicz@need4deed.org>
2025-02-07 09:29:24 +0000

    dummy commit

--- src/hooks/api/useList.tsx
+++ src/hooks/api/useList.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
+
 import fallBackListsEN from "../../components/forms/fallbackLists";
 import fallBackListsDE from "../../components/forms/fallbackListsDE";
 import { urlApiVolunteer } from "../../config/constants";
