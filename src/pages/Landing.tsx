23341e76c2f3e8fd9abfe3dac6b19cf6d87f6ade
Arturas Mickiewicz <arturas.mickiewicz@need4deed.org>
2025-02-20 09:24:26 +0100

    add new design home page

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,9 @@
+import "../styles/styles.css";
+
+export default function Landing() {
+  return (
+    <div className="landing-container">
+      <h1>This is the New Design!</h1>
+    </div>
+  );
+}
