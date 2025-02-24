27d3103048a6ffa2b3fdcf4a6328a93d8a344003
aricionur <aricionur@gmail.com>
2025-02-24 00:06:40 +0000

    Add Icons, Titles and descriptions of cards

--- src/components/VolunteeringCategories/Categories.tsx
+++ src/components/VolunteeringCategories/Categories.tsx
@@ -1,5 +1,55 @@
 import styled from "styled-components";
+import {
+  Baby,
+  Bicycle,
+  CalendarStar,
+  ChatsTeardrop,
+  PingPong,
+  Users,
+} from "@phosphor-icons/react";
 import CategoryCard from "./CategoryCard";
+
+const iconSize = 48;
+const iconColor = "var(--color-papaya)";
+
+const categories = [
+  {
+    icon: <ChatsTeardrop size={iconSize} color={iconColor} />,
+    title: "German Language Support",
+    description:
+      "Fluent in German? Support refugees by teaching german at language cafes and tutoring privately or in groups.",
+  },
+  {
+    icon: <Baby size={iconSize} color={iconColor} />,
+    title: "Childcare",
+    description:
+      "We are looking for volunteers to support children in refugee accommodation centers by assisting in daycare or helping with homework. Experience with children is a plus!",
+  },
+  {
+    icon: <Bicycle size={iconSize} color={iconColor} />,
+    title: "Skills Based Volunteering",
+    description:
+      "Some opportunities may offer a chance to use your special expertise, such as bike repair, gardening, musical skills, or organizational savvy.",
+  },
+  {
+    icon: <CalendarStar size={iconSize} color={iconColor} />,
+    title: "Events",
+    description:
+      "Occasionally events require unique support from volunteers. These might be festivals, dinners, outings, cultural activities, a day of setting up a clothes sorting station, gardening, or a workshop. ",
+  },
+  {
+    icon: <PingPong size={iconSize} color={iconColor} />,
+    title: "Sports activities",
+    description:
+      "We are always looking for volunteers to help us organize sports activities for children, teenagers or adults in accommodation centers.",
+  },
+  {
+    icon: <Users size={iconSize} color={iconColor} />,
+    title: "Accompany a Refugee",
+    description:
+      "Fluent in German and a second language? Support individuals in dealing with bureaucracy at appointments - going with them to the doctor, Job Centre or otherwise.",
+  },
+];
 
 const CategoriesSectionContainer = styled.div`
   display: grid;
@@ -30,12 +80,10 @@ const CategoriesSectionContainer = styled.div`
 function Categories() {
   return (
     <CategoriesSectionContainer>
-      <CategoryCard />
-      <CategoryCard />
-      <CategoryCard />
-      <CategoryCard />
-      <CategoryCard />
-      <CategoryCard />
+      {categories.map((category) => (
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        <CategoryCard key={category.title} {...category} />
+      ))}
     </CategoriesSectionContainer>
   );
 }
--- src/components/VolunteeringCategories/CategoryCard.tsx
+++ src/components/VolunteeringCategories/CategoryCard.tsx
@@ -1,34 +1,53 @@
 import styled from "styled-components";
 
-// const Card = styled.div`
-//   width: 378px;
-//   height: 350px;
-//   border-radius: 24px;
-//   background-color: var(--color-sand);
-// `;
-
 const Card = styled.div`
+  display: flex;
+  flex-direction: column;
   width: 378px;
   height: 350px;
   border-radius: 24px;
   background-color: var(--color-sand);
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  gap: 24px;
+  padding-top: 32px;
+  padding-right: 32px;
+  padding-bottom: 48px;
+  padding-left: 32px;
 `;
 
-const Paragraph = styled.p`
+const Title = styled.h3`
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 30px;
+  letter-spacing: 0.5%;
+  color: var(--color-midnight);
+`;
+
+const Description = styled.p`
   font-weight: 400;
   font-size: 20px;
   line-height: 24px;
   width: 100%;
   color: var(--color-midnight);
+  letter-spacing: 0.5%;
 `;
 
-export default function CategoryCard() {
+const IconDiv = styled.div`
+  height: 48px;
+  width: 48px;
+`;
+
+interface Props {
+  icon: React.ReactNode;
+  title: string;
+  description: string;
+}
+
+export default function CategoryCard({ icon, title, description }: Props) {
   return (
     <Card>
-      <Paragraph>Deneme</Paragraph>
+      <IconDiv>{icon}</IconDiv>
+      <Title>{title}</Title>
+      <Description>{description}</Description>
     </Card>
   );
 }
