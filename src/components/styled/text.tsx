c38efc90d9bdd4beb48190d26a7d8ba53b42a63e
aricionur <aricionur@gmail.com>
2025-03-01 15:41:20 +0000

    Take color as prop for texts

--- src/components/styled/text.tsx
+++ src/components/styled/text.tsx
@@ -1,34 +1,38 @@
 import styled from "styled-components";
 
-export const Heading2 = styled.h2`
+interface TextProps {
+  color?: string;
+}
+
+export const Heading2 = styled.h2<TextProps>`
   font-weight: var(--text-h2-font-weight);
   font-size: var(--text-h2-font-size);
   line-height: var(--text-h2-line-height);
   letter-spacing: var(--text-h2-letter-spacing);
-  color: var(--color-midnight);
+  color: ${(props) => props.color || "var(--color-midnight)"};
 `;
 
-export const Heading3 = styled.h3`
+export const Heading3 = styled.h3<TextProps>`
   font-weight: var(--text-h3-font-weight);
   font-size: var(--text-h3-font-size);
   line-height: var(--text-h3-line-height);
   letter-spacing: var(--text-h3-letter-spacing);
-  color: var(--color-midnight);
+  color: ${(props) => props.color || "var(--color-midnight)"};
 `;
 
-export const Paragraph = styled.p`
+export const Paragraph = styled.p<TextProps>`
   font-weight: var(--text-p-font-weight);
   font-size: var(--text-p-font-size);
   line-height: var(--text-p-line-height);
   letter-spacing: var(--text-p-letter-spacing);
-  color: var(--color-midnight);
+  color: ${(props) => props.color || "var(--color-midnight)"};
 `;
 
-export const ButtonSpan = styled.span`
+export const ButtonSpan = styled.span<TextProps>`
   font-weight: var(--text-button-font-weight);
   font-size: var(--text-button-font-size);
   line-height: var(--text-button-line-height);
   letter-spacing: var(--text-button-letter-spacing);
   text-align: var(--text-button-text-align);
-  color: var(--color-magnolia);
+  color: ${(props) => props.color || "var(--color-magnolia)"};
 `;
