cade2588f909574d2842472327bfb2e55d77a5b3
Arturas Mickiewicz <arturas.mickiewicz@need4deed.org>
2025-03-14 09:13:42 +0000

    fix: rename to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/website",
   test: {
     globals: true,
     environment: "jsdom",
