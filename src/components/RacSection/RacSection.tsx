6844f91cdfe25c54a7205c0cf0e441ab71fc403e
aricionur <aricionur@gmail.com>
2025-05-12 17:07:58 +0100

    Rac section with mock background image

--- src/components/RacSection/RacSection.tsx
+++ src/components/RacSection/RacSection.tsx
@@ -1,7 +1,45 @@
-import { FullWidthContainer } from "../styled/containers";
+import styled from "styled-components";
+import {
+  FullWidthContainer,
+  OverlayingSectionContainer,
+} from "../styled/containers";
 
-export default function RacSection() {
+import { getImageUrl } from "../../utils";
+import { ImageWithGradient } from "../core/image";
+import useScreenType from "../../hooks/useScreenType";
+import { ScreenTypes } from "../../config/types";
+import RacContent from "./Content";
+
+const RacSectionContainer = styled(OverlayingSectionContainer)`
+  height: var(--homepage-rac-section-container-height);
+  justify-content: center;
+  align-items: center;
+`;
+
+const imageNames: Record<ScreenTypes, string> = {
+  mobile: "hero_mobile.webp",
+  tablet: "hero_tablet.webp",
+  desktop: "new-design-hero.webp",
+};
+
+export function RacSection() {
+  const screenType = useScreenType();
+  const imageUrl = getImageUrl(imageNames[screenType]);
+  const gradientClassName = "image-filter-gradient-blue ";
+
   return (
-    <FullWidthContainer id="RAC-Section-FWContainer">onur</FullWidthContainer>
+    <FullWidthContainer id="RACSection-FWContainer">
+      <ImageWithGradient
+        imageUrl={imageUrl}
+        gradientClass={gradientClassName}
+        height="var(--homepage-rac-section-container-height)"
+      />
+
+      <RacSectionContainer id="rac-section-container">
+        <RacContent />
+      </RacSectionContainer>
+    </FullWidthContainer>
   );
 }
+
+export default RacSection;
