c321fcc089444f212ac19f86e3b14c06ed966ab4
Arturas Mickiewicz <arturas.mickiewicz@need4deed.org>
2025-05-31 10:31:42 +0000

    add: header and temp styling

--- src/pages/Opportunities.tsx
+++ src/pages/Opportunities.tsx
@@ -3,44 +3,58 @@ import { useTranslation } from "react-i18next";
 
 import PageLayout from "../components/core/common/PageLayout";
 import OpportunityCards from "../components/OpportunityCards";
+import { Heading2 } from "../components/styled/text";
 import OpportunityCardForGrid from "../components/VolunteeringOpportunities/OpportunityCardForGrid";
 import { urlApiOpportunity } from "../config/constants";
 
 export default function Opportunities() {
-  const { i18n } = useTranslation();
+  const { i18n, t } = useTranslation();
 
   return (
     <PageLayout>
-      <OpportunityCards
-        className="temp-opportunities-container"
-        url={urlApiOpportunity}
-        opportunityParams={{
-          search: {
-            status: ["Volunteers Needed", "Search in process"],
-            opportunity_type: [
-              OpportunityType.GENERAL,
-              OpportunityType.ACCOMPANYING,
-            ],
-          },
-          primaryKeys: ["title", "name"],
-          language: i18n.language as Lang,
+      <div
+        data-testid="opportunities-page-table-tab"
+        // temporary styling for the opportunities page
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "40px",
+          width: "fit-content",
+          marginInline: "auto",
         }}
-        keyMap={{
-          id: "id",
-          type: "opportunity_type",
-          title: "title",
-          languages: "languages",
-          time: "schedule_str",
-          location: "berlin_locations",
-          categoryId: "category_id",
-          activities: "activities",
-          createdAt: "created_at",
-          updatedAt: "updated_at",
-          accompDate: "accomp_datetime",
-          vo: "vo_information",
-        }}
-        CardComponent={OpportunityCardForGrid}
-      />
+      >
+        <Heading2>{t("opportunityPage.header")}</Heading2>
+        <OpportunityCards
+          className="temp-opportunities-container"
+          url={urlApiOpportunity}
+          opportunityParams={{
+            search: {
+              status: ["Volunteers Needed", "Search in process"],
+              opportunity_type: [
+                OpportunityType.GENERAL,
+                OpportunityType.ACCOMPANYING,
+              ],
+            },
+            primaryKeys: ["title", "name"],
+            language: i18n.language as Lang,
+          }}
+          keyMap={{
+            id: "id",
+            type: "opportunity_type",
+            title: "title",
+            languages: "languages",
+            time: "schedule_str",
+            location: "berlin_locations",
+            categoryId: "category_id",
+            activities: "activities",
+            createdAt: "created_at",
+            updatedAt: "updated_at",
+            accompDate: "accomp_datetime",
+            vo: "vo_information",
+          }}
+          CardComponent={OpportunityCardForGrid}
+        />
+      </div>
     </PageLayout>
   );
 }
