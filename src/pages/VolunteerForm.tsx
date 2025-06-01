89384a180668fc4f74c789dd1ce5c55c659b7ed5
Arturas Mickiewicz <arturas.mickiewicz@need4deed.org>
2025-06-01 22:09:20 +0000

    fix: styling

--- src/pages/VolunteerForm.tsx
+++ src/pages/VolunteerForm.tsx
@@ -4,7 +4,7 @@ import { StaticPageLayout } from "../components/Layouts/staticPageLayout";
 
 export default function VolunteerForm() {
   return (
-    <StaticPageLayout>
+    <StaticPageLayout background="var(--color-white)">
       <Form form={FormType.VOLUNTEER} />
     </StaticPageLayout>
   );
