695ab1cb95130d2bbdffbb2ebf23937c3e8a8811
Arturas Mickiewicz <arturas.mickiewicz@need4deed.org>
2025-06-01 17:10:11 +0000

    add: icon component

---
+++ src/components/svg/CloseIcon.tsx
@@ -0,0 +1,19 @@
+export default function CloseIcon() {
+  return (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M3.40351 2L12 10.5965L20.5965 2L22 3.40351L13.4035 12L22 20.5965L20.5965 22L12 13.4035L3.40351 22L2 20.5965L10.5965 12L2 3.40351L3.40351 2Z"
+        fill="#1A2684"
+      />
+    </svg>
+  );
+}
