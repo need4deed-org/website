f383a6196a9621cb1a82419ea6da6924e25f8388
aricionur <aricionur@gmail.com>
2025-06-02 15:52:51 +0100

    update the sanity check sentence accroding to new home page

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,14 +1,11 @@
-// import { render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
-// import App from "./App";
+import App from "./App";
 
-// Todo: refactor  this test according to new home page later !
 describe("sanity", () => {
   it("should NEED 4 DEED be visible", () => {
-    // render(<App />);
-    // const textN4D = screen.queryByText(
-    //   /Need based voluntary support for refugees in Berlin/,
-    // );
-    // expect(textN4D).toBeVisible();
+    render(<App />);
+    const textN4D = screen.queryByText(/Voluntary support for refugees/);
+    expect(textN4D).toBeVisible();
   });
 });
