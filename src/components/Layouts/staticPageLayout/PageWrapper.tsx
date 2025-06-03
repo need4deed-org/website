b9cb4f16bc9e6e2cf50e978de7fe2e7c99cd3db6
Arturas Mickiewicz <arturas.mickiewicz@need4deed.org>
2025-06-03 09:20:54 +0000

    refactor: rename wrapper and move scrolling to it

--- src/components/Layouts/staticPageLayout/PageWrapper.tsx
+++ src/components/Layouts/staticPageLayout/PageWrapper.tsx
@@ -1,20 +1,22 @@
 import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
-import { useSearchParams } from "react-router-dom";
+import { useLocation, useSearchParams } from "react-router-dom";
 
 interface Props extends React.PropsWithChildren {}
 
-export default function WidthLanguageQueryParam({
-  children,
-}: Props): React.ReactElement {
+export default function PageWrapper({ children }: Props): React.ReactElement {
   const { i18n } = useTranslation();
   const [queryParams] = useSearchParams();
   const language = queryParams.get("language");
+  const location = useLocation();
 
   useEffect(() => {
     i18n.changeLanguage(language || "en");
   }, [i18n, language]);
 
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
   // eslint-disable-next-line react/jsx-no-useless-fragment
   return <>{children}</>;
 }
