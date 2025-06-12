2d4c84374637216b753fa87e06b3a67a6f1e5094
aricionur <aricionur@gmail.com>
2025-06-12 15:58:19 +0100

    Add cursor pointer on hover effect to the n4d logo

--- src/components/Layouts/staticPageLayout/logos/N4DLogo.tsx
+++ src/components/Layouts/staticPageLayout/logos/N4DLogo.tsx
@@ -10,18 +10,14 @@ export function N4DLogo() {
   ).getPropertyValue("--layout-static-page-n4d-logo-height");
 
   return (
-    // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
-    <span
+    <N4DLogoFlat
+      color="var(--color-orchid-dark)"
+      width={logoWidth}
+      height={logoHeight}
       onClick={() => {
         window.location.href = "/";
       }}
-    >
-      <N4DLogoFlat
-        color="var(--color-orchid-dark)"
-        width={logoWidth}
-        height={logoHeight}
-      />
-    </span>
+    />
   );
 }
 
