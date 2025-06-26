b3ca9f59397bcbea6143b6c64c23552100fe67ec
aricionur <aricionur@gmail.com>
2025-06-26 10:20:54 +0100

    Remove unnecessary lang switch container

--- src/components/HeaderNew/LanguageSwitcher.tsx
+++ src/components/HeaderNew/LanguageSwitcher.tsx
@@ -1,17 +1,8 @@
 import { Lang } from "need4deed-sdk";
 import { useTranslation } from "react-i18next";
-import styled from "styled-components";
 
 import { setStoredLang } from "../../utils";
 import MenuItem from "./MenuItem";
-
-const LanguageSwitcherContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  background: transparent;
-  gap: var(--homepage-hero-section-language-switcher-gap);
-  width: var(--homepage-hero-section-language-switcher-width);
-`;
 
 const en = "ENGLISH";
 const de = "DEUTSCH";
@@ -29,14 +20,12 @@ export default function LanguageSwitcher({ textColor }: Props) {
   };
 
   return (
-    <LanguageSwitcherContainer>
-      <MenuItem
-        text={i18n.language === Lang.EN ? de : en}
-        color={textColor}
-        onClick={() =>
-          handleLangChange(i18n.language === Lang.EN ? Lang.DE : Lang.EN)
-        }
-      />
-    </LanguageSwitcherContainer>
+    <MenuItem
+      text={i18n.language === Lang.EN ? de : en}
+      color={textColor}
+      onClick={() =>
+        handleLangChange(i18n.language === Lang.EN ? Lang.DE : Lang.EN)
+      }
+    />
   );
 }
