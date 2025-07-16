5ad0bc33f5260e59cace46e2aab31ce550ac8c0f
esraaEmad <esso.em.96@gmail.com>
2025-07-16 17:37:37 +0200

    revert test

--- src/components/Legal/DataPrivacy.tsx
+++ src/components/Legal/DataPrivacy.tsx
@@ -141,7 +141,6 @@ function DataPrivacy() {
             <p>{t("legal:dataPrivacy.accordion-5.h-3")}</p>
             <h4>{t("legal:dataPrivacy.accordion-5.head4")}</h4>
             <p>{t("legal:dataPrivacy.accordion-5.h-4")}</p>
-            <p>test</p>
           </Accordion.Body>
         </Accordion>
       </div>
