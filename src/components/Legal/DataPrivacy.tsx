77bf1e514a2aef4d97f78733b0696a8ee109d453
esraaEmad <esso.em.96@gmail.com>
2025-07-16 16:45:54 +0200

    section-5

--- src/components/Legal/DataPrivacy.tsx
+++ src/components/Legal/DataPrivacy.tsx
@@ -28,7 +28,7 @@ function DataPrivacy() {
             <a href="https://www.club-dialog.de/">
               {t("legal:dataPrivacy.accordion-1.link1")}
               <span className="accordion-header">
-                {t("legal:dataPrivacy.accordion-1.link1")}
+                {t("legal:dataPrivacy.accordion-1.link2")}
               </span>
             </a>
           </Accordion.Body>
@@ -77,6 +77,70 @@ function DataPrivacy() {
             <p>{t("legal:dataPrivacy.accordion-3.h-3")}</p>
             <h4>{t("legal:dataPrivacy.accordion-3.head4")}</h4>
             <p>{t("legal:dataPrivacy.accordion-3.h-4")}</p>
+          </Accordion.Body>
+        </Accordion>
+        <Accordion>
+          <Accordion.Header>
+            <h2>
+              <span className="accordion-header">04 </span>
+              {t("legal:dataPrivacy.accordion-4.header")}
+            </h2>
+          </Accordion.Header>
+          <Accordion.Body>
+            <h4>Description and Scope</h4>
+            <p>{t("legal:dataPrivacy.accordion-4.part1")}</p>
+            <br />
+            <p>{t("legal:dataPrivacy.accordion-4.part2")}</p>
+            <br />
+            <p>
+              <b className="accordion-header"> ━ </b> &nbsp; &nbsp; &nbsp;
+              {t("legal:dataPrivacy.accordion-4.part3")}
+            </p>
+            <p>
+              <b className="accordion-header"> ━ </b> &nbsp; &nbsp; &nbsp;
+              {t("legal:dataPrivacy.accordion-4.part4")}
+            </p>
+            <p>
+              <b className="accordion-header"> ━ </b> &nbsp; &nbsp; &nbsp;
+              {t("legal:dataPrivacy.accordion-4.part5")}
+            </p>
+            <p>
+              <b className="accordion-header"> ━ </b> &nbsp; &nbsp; &nbsp;
+              {t("legal:dataPrivacy.accordion-4.part6")}
+            </p>
+            <p>
+              <b className="accordion-header"> ━ </b> &nbsp; &nbsp; &nbsp;
+              {t("legal:dataPrivacy.accordion-4.part7")}
+            </p>
+
+            <h4>{t("legal:dataPrivacy.accordion-4.head1")}</h4>
+            <p>{t("legal:dataPrivacy.accordion-4.h-1")}</p>
+            <h4>{t("legal:dataPrivacy.accordion-4.head2")}</h4>
+            <p>{t("legal:dataPrivacy.accordion-4.h-2")}</p>
+            <h4>{t("legal:dataPrivacy.accordion-4.head3")}</h4>
+            <p>{t("legal:dataPrivacy.accordion-4.h-3")}</p>
+            <h4>{t("legal:dataPrivacy.accordion-4.head4")}</h4>
+            <p>{t("legal:dataPrivacy.accordion-4.h-4")}</p>
+          </Accordion.Body>
+        </Accordion>
+        <Accordion>
+          <Accordion.Header>
+            <h2>
+              <span className="accordion-header">05 </span>
+              {t("legal:dataPrivacy.accordion-5.header")}
+            </h2>
+          </Accordion.Header>
+          <Accordion.Body>
+            <h4>Description and Scope</h4>
+            <p>{t("legal:dataPrivacy.accordion-5.part1")}</p>
+            <h4>{t("legal:dataPrivacy.accordion-5.head1")}</h4>
+            <p>{t("legal:dataPrivacy.accordion-5.h-1")}</p>
+            <h4>{t("legal:dataPrivacy.accordion-5.head2")}</h4>
+            <p>{t("legal:dataPrivacy.accordion-5.h-2")}</p>
+            <h4>{t("legal:dataPrivacy.accordion-5.head3")}</h4>
+            <p>{t("legal:dataPrivacy.accordion-5.h-3")}</p>
+            <h4>{t("legal:dataPrivacy.accordion-5.head4")}</h4>
+            <p>{t("legal:dataPrivacy.accordion-5.h-4")}</p>
           </Accordion.Body>
         </Accordion>
       </div>
