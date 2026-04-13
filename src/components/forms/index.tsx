d10730e1c8d2aa31c2dd47ae3c27fec33db0d859
arturasmckwcz <arturasmckwcz@gmail.com>
2026-04-13 08:56:02 +0000

    fix: rollback to local forms

--- src/components/forms/index.tsx
+++ src/components/forms/index.tsx
@@ -1,6 +1,5 @@
-import { Lang } from "need4deed-sdk";
-import { useTranslation } from "react-i18next";
-import { getExternalUrl } from "../../utils";
+import AddOpportunity from "./AddOpportunity";
+import BecomeVolunteer from "./BecomeVolunteer";
 import { FormType } from "./types";
 
 interface Props {
@@ -8,9 +7,12 @@ interface Props {
 }
 
 export default function Form({ form }: Props) {
-  const { i18n } = useTranslation();
-
-  const to = getExternalUrl(i18n.language as Lang, form);
-  window.location.replace(to);
-  return null;
+  switch (form) {
+    case FormType.VOLUNTEER:
+      return <BecomeVolunteer />;
+    case FormType.OPPORTUNITY:
+      return <AddOpportunity />;
+    default:
+      return null;
+  }
 }
